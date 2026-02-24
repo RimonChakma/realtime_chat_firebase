858612fcaa6f761c97f0fd60e5cebee4e045f1ef
Rimon Chakma <rimon.chakma880@gmail.com>
2026-02-24 19:55:00 +0600

    add chatprovider

--- lib/auth_provider.dart
+++ lib/auth_provider.dart
@@ -3,28 +3,41 @@ import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AuthProvider extends ChangeNotifier{
+class AuthProvider extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   User? get currentUser => _auth.currentUser;
+
   bool? get isSignedIn => currentUser != null;
 
-  Future<void> signIn (String email, String password) async {
+  Future<void> signIn(String email, String password) async {
     await _auth.signInWithEmailAndPassword(email: email, password: password);
     notifyListeners();
   }
 
-  Future<void> signUp (String email, String password, String name ,String imageUrl) async {
-    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  Future<void> signUp(
+    String email,
+    String password,
+    String name,
+    String imageUrl,
+  ) async {
+    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
     await _firestore.collection('users').doc(userCredential.user!.uid).set({
-      "uid":userCredential.user!.uid,
+      "uid": userCredential.user!.uid,
       "name": name,
-      "email":email,
-      "imageUrl":imageUrl
+      "email": email,
+      "imageUrl": imageUrl,
     });
 
     notifyListeners();
   }
 
+  Future<void> signOut () async {
+    await _auth.signOut();
+    notifyListeners();
+  }
 }
